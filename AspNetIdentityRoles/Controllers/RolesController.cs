b98e29881b0268839a8c6a31d06af3321b74e7e3
ywl20013 <ywl20013@126.com>
2015-06-30 17:10:33 +0800

    初步调试成功

--- AspNetIdentityRoles/Controllers/RolesController.cs
+++ AspNetIdentityRoles/Controllers/RolesController.cs
@@ -13,6 +13,8 @@ namespace AspNetIdentityRoles.Controllers
     {
         private ApplicationDbContext _db = new ApplicationDbContext();
 
+        //验证登录用户角色="Admin"或者用户名="Admin"
+        [Authorize(Roles = "Admin", Users = "Admin")]
         public ActionResult Index()
         {
             var rolesList = new List<RoleViewModel>();
@@ -25,7 +27,7 @@ namespace AspNetIdentityRoles.Controllers
         }
 
 
-        //[Authorize(Roles = "Admin")]
+        [Authorize(Roles = "Admin", Users = "Admin")]
         public ActionResult Create(string message = "")
         {
             ViewBag.Message = message;
@@ -33,7 +35,7 @@ namespace AspNetIdentityRoles.Controllers
         }
 
         [HttpPost]
-        //[Authorize(Roles = "Admin")]
+        [Authorize(Roles = "Admin", Users = "Admin")]
         public ActionResult Create([Bind(Include =
             "RoleName,Category,Description")]RoleViewModel model)
         {
@@ -50,14 +52,14 @@ namespace AspNetIdentityRoles.Controllers
                 else
                 {
                     idManager.CreateRole(model.RoleName, model.Description);
-                    return RedirectToAction("Index", "Account");
+                    return RedirectToAction("Index");
                 }
             }
             return View();
         }
 
 
-        [Authorize(Roles = "Admin")]
+        [Authorize(Roles = "Admin", Users = "Admin")]
         public ActionResult Edit(string id)
         {
             // It's actually the Role.Name tucked into the id param:
@@ -68,7 +70,7 @@ namespace AspNetIdentityRoles.Controllers
 
 
         [HttpPost]
-        [Authorize(Roles = "Admin")]
+        [Authorize(Roles = "Admin", Users = "Admin")]
         public ActionResult Edit([Bind(Include =
             "RoleName,Category,OriginalRoleName,Description")] EditRoleViewModel model)
         {
@@ -86,7 +88,7 @@ namespace AspNetIdentityRoles.Controllers
         }
 
 
-        [Authorize(Roles = "Admin")]
+        [Authorize(Roles = "Admin", Users = "Admin")]
         public ActionResult Delete(string id)
         {
             if (id == null)
@@ -103,7 +105,7 @@ namespace AspNetIdentityRoles.Controllers
         }
 
 
-        [Authorize(Roles = "Admin")]
+        [Authorize(Roles = "Admin", Users = "Admin")]
         [HttpPost, ActionName("Delete")]
         public ActionResult DeleteConfirmed(string id)
         {
